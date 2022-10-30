f73f610ed28cad925d6e3e0dab6fd8b2a70639f1
SunYoung-P <p2sy45@gmail.com>
2022-10-30 20:41:43 +0900

    객체 포인터 변수와 같은 속성을 지니는 참조자 관련 학습 추가

---
+++ C_PLUS_PLUS/BOOK_PART_02/Inheritance/ReferenceAttribute.cpp
@@ -0,0 +1,48 @@
+
+#include <iostream>
+using namespace std;
+
+class cc1030
+{
+public:
+	void FirstFunc() { cout << "FirstFunc()" << endl; }
+	virtual void SimpleFunc() { cout << "First's SimpleFunc()" << endl; }
+};
+
+class ss1030 : public cc1030
+{
+public:
+	void SecondFunc() { cout << "SecondFunc()" << endl; }
+	virtual void SimpleFunc() { cout << "Second's SimpleFunc()" << endl; }
+};
+
+class dd1030 : public ss1030
+{
+public:
+	void ThirdFunc() { cout << "ThirdFunc()" << endl; }
+	virtual void SimpleFunc() { cout << "Third's SimpleFunc()" << endl; }
+};
+
+int ReferenceAttribute()
+{
+	dd1030 obj;
+	obj.FirstFunc();
+	obj.SecondFunc();
+	obj.ThirdFunc();
+	obj.SimpleFunc();
+
+	cout << endl << endl;
+
+	ss1030 & sref = obj;
+	sref.FirstFunc();
+	sref.SecondFunc();
+	sref.SimpleFunc();
+
+	cout << endl << endl;
+
+	cc1030 & fref = obj;
+	fref.FirstFunc();
+	fref.SimpleFunc();
+
+	return 0;
+}
