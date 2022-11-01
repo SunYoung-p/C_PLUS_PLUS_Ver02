8a556efa5f5ae292b1f3eb3b4732a55d7fc725f3
SunYoung-P <p2sy45@gmail.com>
2022-11-02 01:24:29 +0900

    피드백 추가 11/02  1. OOP 단계별 프로젝트로 Account 클래스를 상속하는 2개의 클래스를 만들고, 계좌 입금 시 이자가 붙는 방식이 다르므로 입금 함수를 가상 함수로 만들어서 적용했는데, 가상 함수 구현 부분에서 기초 클래스의 입금 함수를 가져와서 쓰면 편한데,  입금 관련 코드를 그냥 새로 만들었다. 상속 후 가상함수 설정은 잘 되었지만, 상속한 기초 클래스의 멤버 함수를 쓰는 게 아직 익숙하지 않다.

--- C_PLUS_PLUS/PROJECT/AccountClasses.cpp
+++ C_PLUS_PLUS/PROJECT/AccountClasses.cpp
@@ -1,3 +1,10 @@
+/*
+11/02
+1. OOP 단계별 프로젝트로 Account 클래스를 상속하는 2개의 클래스를 만들고, 계좌 입금 시 이자가 붙는 방식이 다르므로
+입금 함수를 가상 함수로 만들어서 적용했는데, 가상 함수 구현 부분에서
+기초 클래스의 입금 함수를 가져와서 쓰면 편한데,  입금 관련 코드를 그냥 새로 만들었다.
+상속 후 가상함수 설정은 잘 되었지만, 상속한 기초 클래스의 멤버 함수를 쓰는 게 아직 익숙하지 않다.
+*/
 #include <iostream>
 #include "AccountClasses.h"
 using namespace std;
@@ -41,6 +48,7 @@ int HighCreditAccount::GetMoney() const
 
 void HighCreditAccount::PlusMoney(int money)
 {
+	
 	switch (level)
 	{
 		case 1: ratio += AccountRatio::LEVEL_A; break;
